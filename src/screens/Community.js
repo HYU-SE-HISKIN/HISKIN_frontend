bec3415044b49311bd79a0934e2dfdbcd4cfa6b3
Information Systems <infosys@Informations-MacBook-Air.local>
2023-11-20 22:10:46 +0900

    add scroll view in Community.js

--- src/screens/Community.js
+++ src/screens/Community.js
@@ -1,8 +1,6 @@
-import React, { useState, useRef, useContext } from "react";
-import styled, { View, Text } from "styled-components/native";
-import { UserContext } from "../contexts/User";
-import axios from "axios";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import React from "react";
+import styled from "styled-components/native";
+import { ScrollView } from "react-native";
 import { EmptyBox, PlusButton, CommunityBox } from "../components";
 import { EditButton } from "../../assets/images";
 
@@ -33,57 +31,59 @@ const IvoryContainer = styled.View`
 
 const Community = ({ navigation }) => {
   return (
-    <Container>
-      <AppName>HISKIN</AppName>
-      <IvoryContainer>
-        <PlusButton
-          thirdtitle="어떤 생각을 하고 있나요?"
-          onPress={() => navigation.navigate("DeviceRegistration")}
+    <ScrollView>
+      <Container>
+        <AppName>HISKIN</AppName>
+        <IvoryContainer>
+          <PlusButton
+            thirdtitle="어떤 생각을 하고 있나요?"
+            onPress={() => navigation.navigate("DeviceRegistration")}
+          />
+          <EditButton />
+        </IvoryContainer>
+        <EmptyBox height={12} />
+        <CommunityBox
+          color="pink"
+          title={"hi_skin0032"}
+          subtitle={"12 minutes ago"}
+          content="아름다운 걸 아름답다고, 사랑을 사랑이라고 있는 그대로 말할 수 있는 사람들이 좋다. 가만 보면 참 쉬운 일 같은데 또 이만큼 어려운 일이 없는가 보다."
+          heartvalue=" 27"
         />
-        <EditButton />
-      </IvoryContainer>
-      <EmptyBox height={12} />
-      <CommunityBox
-        color="pink"
-        title={"hi_skin0032"}
-        subtitle={"12 minutes ago"}
-        content="아름다운 걸 아름답다고, 사랑을 사랑이라고 있는 그대로 말할 수 있는 사람들이 좋다. 가만 보면 참 쉬운 일 같은데 또 이만큼 어려운 일이 없는가 보다."
-        heartvalue=" 27"
-      />
-      <EmptyBox height={12} />
-      <CommunityBox
-        color="yellow"
-        title={"hanyang2220"}
-        subtitle={"17 minutes ago"}
-        image={true}
-        content="Save yourself :)"
-        heartvalue=" 80"
-      />
-      <EmptyBox height={12} />
-      <CommunityBox
-        color="skyblue"
-        title={"hihihihihi"}
-        subtitle={"19 minutes ago"}
-        content="To many people in the class"
-        heartvalue=" 15"
-      />
-      <EmptyBox height={12} />
-      <CommunityBox
-        color="purple"
-        title={"hi_skin0032"}
-        subtitle={"30 minutes ago"}
-        content="아름다운 걸 아름답다고, 사랑을 사랑이라고 있는 그대로 말할 수 있는 사람들이 좋다. 가만 보면 참 쉬운 일 같은데 또 이만큼 어려운 일이 없는가 보다."
-        heartvalue=" 27"
-      />
-      <EmptyBox height={12} />
-      <CommunityBox
-        color="red"
-        title={"hi_skin0032"}
-        subtitle={"55 minutes ago"}
-        content="아름다운 걸 아름답다고, 사랑을 사랑이라고 있는 그대로 말할 수 있는 사람들이 좋다. 가만 보면 참 쉬운 일 같은데 또 이만큼 어려운 일이 없는가 보다."
-        heartvalue=" 27"
-      />
-    </Container>
+        <EmptyBox height={12} />
+        <CommunityBox
+          color="yellow"
+          title={"hanyang2220"}
+          subtitle={"17 minutes ago"}
+          image={true}
+          content="Save yourself :)"
+          heartvalue=" 80"
+        />
+        <EmptyBox height={12} />
+        <CommunityBox
+          color="skyblue"
+          title={"hihihihihi"}
+          subtitle={"19 minutes ago"}
+          content="To many people in the class"
+          heartvalue=" 15"
+        />
+        <EmptyBox height={12} />
+        <CommunityBox
+          color="purple"
+          title={"hi_skin0032"}
+          subtitle={"30 minutes ago"}
+          content="아름다운 걸 아름답다고, 사랑을 사랑이라고 있는 그대로 말할 수 있는 사람들이 좋다. 가만 보면 참 쉬운 일 같은데 또 이만큼 어려운 일이 없는가 보다."
+          heartvalue=" 27"
+        />
+        <EmptyBox height={12} />
+        <CommunityBox
+          color="red"
+          title={"hi_skin0032"}
+          subtitle={"55 minutes ago"}
+          content="아름다운 걸 아름답다고, 사랑을 사랑이라고 있는 그대로 말할 수 있는 사람들이 좋다. 가만 보면 참 쉬운 일 같은데 또 이만큼 어려운 일이 없는가 보다."
+          heartvalue=" 27"
+        />
+      </Container>
+    </ScrollView>
   );
 };
 

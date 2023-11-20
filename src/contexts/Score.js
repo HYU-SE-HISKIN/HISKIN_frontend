2f2c404e20979ef173bbe165dd3b1e84e72097d6
Information Systems <infosys@Informations-MacBook-Air.local>
2023-11-20 21:13:35 +0900

    add ScoreContext

---
+++ src/contexts/Score.js
@@ -0,0 +1,19 @@
+import React, { useState, createContext } from "react";
+
+const ScoreContext = createContext({
+  score: null,
+  dispatch: () => {},
+});
+
+const ScoreProvider = ({ children }) => {
+  const [score, setScore] = useState(0);
+  const dispatch = ({ inputScore }) => {
+    setUser({ inputScore });
+  };
+  const value = { score, dispatch };
+  return (
+    <ScoreContext.Provider value={value}>{children}</ScoreContext.Provider>
+  );
+};
+
+export { ScoreContext, ScoreProvider };
---
+++ src/contexts/index.js
@@ -0,0 +1,4 @@
+import { UserContext, UserProvider } from "./User";
+import { ScoreContext, ScoreProvider } from "./Score";
+
+export { UserContext, UserProvider, ScoreContext, ScoreProvider };

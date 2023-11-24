b543fff53a48df5efa6aad4a1c1c64300741fddc
ohhoho8 <haeryung8@gmail.com>
2023-11-24 14:16:32 +0900

    update: CareDetail screen

--- src/screens/CareDetails.js
+++ src/screens/CareDetails.js
@@ -1,18 +1,8 @@
 import React, { useState, useRef, useContext } from "react";
 import styled from "styled-components/native";
 import { View } from "react-native";
-import { UserContext } from "../contexts/User";
-import axios from "axios";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { EmptyBox, PlusButton } from "../components";
-import {
-  AppName_small,
-  NextButton,
-  IconAccount,
-  IconChallenge,
-  IconReport,
-  IconSetting,
-} from "../../assets/images";
+import { EmptyBox, PlusButton, WhiteContainer } from "../components";
+import { NextButton } from "../../assets/images";
 
 const Container = styled.View`
   flex: 1;
@@ -20,68 +10,20 @@ const Container = styled.View`
   background-color: ${({ theme }) => theme.ivory_0};
   padding: 0 18px;
 `;
-const HorizonContainer = styled.View`
-  flex-direction: row;
-`;
-const TabName = styled.Text`
-  font-family: "LG Smart - Regular";
-  font-size: 14px;
-  color: ${({ theme }) => theme.black};
-  align-self: flex-end;
-`;
-const TitleText = styled.Text`
-  font-family: "LG EI Text - SemiBold";
-  font-size: 18px;
-  color: ${({ theme }) => theme.black};
-  align-self: flex-start;
-`;
-const WhiteContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  background-color: ${({ theme }) => theme.white};
-  border-radius: 24px;
-  padding: 24px;
-  width: 354px;
-  height: 80px;
-`;
 
-const CareDetails = ({ navigation }) => {
+const CareDetails = ({ route, navigation }) => {
+  const { buttonName } = route.params;
+  const name = buttonName.split(" ").slice(-2, -1)[0];
+
   return (
     <Container>
-      <EmptyBox height={64} />
-      <HorizonContainer>
-        <AppName_small />
-        <View>
-          <EmptyBox height={15} />
-          <TabName> myinfo</TabName>
-        </View>
-      </HorizonContainer>
-      <EmptyBox height={20} />
-      <TitleText>내 정보</TitleText>
-      <EmptyBox height={20} />
-      <WhiteContainer>
-        <IconAccount />
-        <PlusButton title=" 계정 설정" isFrontImage={true} />
+      <WhiteContainer height={80} borderRadius={24}>
+        <PlusButton title={name + " 케어 루틴  1"} />
         <NextButton />
       </WhiteContainer>
       <EmptyBox height={10} />
-      <WhiteContainer>
-        <IconSetting />
-        <PlusButton
-          title="피부 타입 설정"
-          isFrontImage={true}
-          onPress={() => navigation.navigate("SkinTypeTest")}
-        />
-        <NextButton />
-      </WhiteContainer>
-      <EmptyBox height={10} />
-      <WhiteContainer>
-        <IconReport />
-        <PlusButton
-          title="피부 리포트"
-          isFrontImage={true}
-          onPress={() => navigation.navigate("SkinReport")}
-        />
+      <WhiteContainer height={80} borderRadius={24}>
+        <PlusButton title={name + " 케어 루틴  2"} />
         <NextButton />
       </WhiteContainer>
     </Container>

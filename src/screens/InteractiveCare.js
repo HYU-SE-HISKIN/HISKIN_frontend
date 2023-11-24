206cb971c07eaf2af46a0dcd42c6773d3f7a9f63
ohhoho8 <haeryung8@gmail.com>
2023-11-24 19:45:35 +0900

    fix: lottie and outline circle

--- src/screens/InteractiveCare.js
+++ src/screens/InteractiveCare.js
@@ -1,26 +1,45 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { View } from "react-native";
 import styled from "styled-components/native";
 import LottieView from "lottie-react-native";
 
 const Container = styled.View`
+  flex: 1;
+  justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.ivory_0};
-  padding: 0 18px;
+`;
+
+const AnimatedContainer = styled.View`
+  position: relative;
+`;
+
+const OutlineCircle = styled.View`
+  position: absolute;
+  align-self: center;
+  width: 74px;
+  height: 74px;
+  border-radius: 37px;
+  border-width: 5px;
+  border-color: #fff;
+  margin-top: 30px;
 `;
 
 const InteractiveCare = ({ navigation }) => {
   return (
     <Container>
-      <LottieView
-        style={{
-          width: 87,
-          height: 87,
-        }}
-        source={require("../../assets/lottie/AnimationCircle.json")}
-        autoPlay
-        loop={true}
-      />
+      <AnimatedContainer>
+        <OutlineCircle />
+        <LottieView
+          style={{
+            width: 150,
+            height: 136,
+          }}
+          source={require("../../assets/lottie/AnimationCircle.json")}
+          autoPlay
+          loop={true}
+        />
+      </AnimatedContainer>
     </Container>
   );
 };

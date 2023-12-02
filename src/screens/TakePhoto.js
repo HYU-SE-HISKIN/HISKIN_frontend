0fe95546091ec72900dd616ac386ddc82ffca54c
ohhoho8 <haeryung8@gmail.com>
2023-12-02 17:33:05 +0900

    update: cameragit add . get real camera

--- src/screens/TakePhoto.js
+++ src/screens/TakePhoto.js
@@ -1,77 +1,145 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Camera } from "expo-camera";
-import * as ImagePicker from "expo-image-picker";
+import styled from "styled-components/native";
+import { Text, Image } from "react-native";
+import Constants from "expo-constants";
+import { Camera, CameraType } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
-import { Button } from "../components";
-import { View, Alert, Platform } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-const TakePhoto = () => {
-  const [photos, setPhotos] = useState([]);
-  const camera = useRef();
-  const [cameraReady, setCameraReady] = useState(false);
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding-top: ${Constants.statusBarHeight}px;
+  background-color: #000;
+  padding: 8px;
+`;
 
-  const getPermissions = async () => {
-    const { granted } = await Camera.requestPermissionsAsync();
-    setOk(granted);
-  };
+const Controls = styled.View`
+  flex: 0.5;
+`;
+
+const Button = styled.TouchableOpacity`
+  height: 40px;
+  border-radius: 6px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CameraContainer = styled.View`
+  flex: 5;
+  border-radius: 20px;
+`;
+
+const HorizonContainer = styled.View`
+  flex-direction: row;
+`;
+
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
   useEffect(() => {
-    getPermissions();
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
   }, []);
 
-  const onCameraReady = () => setCameraReady(true);
   const takePicture = async () => {
-    if (camera.current && cameraReady) {
-      const photo = await camera.current.takePictureAsync({
-        quality: 1,
-        exif: true,
-      });
-    }
-  };
-
-  const onCameraSwitch = () => {
-    if (cameraType === Camera.Constants.Type.front) {
-      setCameraType(Camera.Constants.Type.back);
-    } else {
-      setCameraType(Camera.Constants.Type.front);
-    }
-  };
-
-  const getPhotos = async () => {
-    const { assets: photos } = await MediaLibrary.getAssetsAsync();
-    setPhotos(photos);
-    setChosenPhoto(photos[0]?.uri);
-  };
-  const pickImage = async () => {
-    const { status, canAskAgain } = await MediaLibrary.getPermissionsAsync();
-
-    if (status === "undetermined" && canAskAgain) {
-      const { status } = await MediaLibrary.requestPermissionsAsync();
-      if (status !== "undetermined") {
-        setOk(true);
-        getPhotos();
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setImage(data.uri);
+      } catch (error) {
+        console.log(error);
       }
-    } else if (status !== "undetermined") {
-      setOk(true);
-      getPhotos();
     }
   };
+
+  const savePicture = async () => {
+    if (image) {
+      try {
+        const asset = await MediaLibrary.createAssetAsync(image);
+        alert("Picture saved! 🎉");
+        setImage(null);
+        console.log("saved successfully");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
-    <View>
-      <Camera
-        type={cameraType}
-        style={{ flex: 1 }}
-        zoom={zoom}
-        flashMode={flashMode}
-        ref={camera}
-        onCameraReady={onCameraReady}
-      />
+    <Container>
+      {!image ? (
+        <CameraContainer>
+          <Camera
+            style={{ flex: 1 }}
+            type={type}
+            ref={cameraRef}
+            flashMode={flash}
+          />
+          <HorizonContainer>
+            <Button
+              onPress={() => {
+                setType(
+                  type === CameraType.back ? CameraType.front : CameraType.back
+                );
+              }}
+            >
+              <MaterialIcons name="cached" size={24} color="white" />
+            </Button>
+            <Button
+              onPress={() =>
+                setFlash(
+                  flash === Camera.Constants.FlashMode.off
+                    ? Camera.Constants.FlashMode.on
+                    : Camera.Constants.FlashMode.off
+                )
+              }
+            >
+              <MaterialIcons
+                name={
+                  flash === Camera.Constants.FlashMode.off
+                    ? "flash-on"
+                    : "flash-off"
+                }
+                size={24}
+                color="white"
+              />
+            </Button>
+          </HorizonContainer>
+        </CameraContainer>
+      ) : (
+        <Image source={{ uri: image }} style={{ flex: 1, borderRadius: 20 }} />
+      )}
 
-      <Button title="Toggle Camera Type" onPress={onCameraSwitch}></Button>
-      <Button title="Take Picture" onPress={takePicture}></Button>
-      <Button title="Pick Image" onPress={pickImage}></Button>
-    </View>
+      <Controls>
+        {image ? (
+          <HorizonContainer>
+            <Button onPress={() => setImage(null)}>
+              <MaterialIcons name="cached" size={24} color="white" />
+            </Button>
+            <Button onPress={savePicture}>
+              <MaterialIcons name="check" size={24} color="white" />
+            </Button>
+          </HorizonContainer>
+        ) : (
+          <Button onPress={takePicture}>
+            <MaterialIcons name="camera" size={24} color="white" />
+          </Button>
+        )}
+      </Controls>
+    </Container>
   );
-};
-
-export default TakePhoto;
+}
